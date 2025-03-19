a3333ae76f2ec49df6a664ae60ab05c0e9a505e2
SolangeFaria <solange_tenerife@hotmail.com>
2025-03-19 12:50:57 +0000

    Creacion De Rama Sol

--- SignalRConsola/modules/hash/Hash.cs
+++ SignalRConsola/modules/hash/Hash.cs
@@ -1,11 +1,58 @@
+using misFunciones;
+using SignalRConsola.modules.signalR;
+
 namespace SignalRConsola.modules.hash;
 class Hash
 {
     public void Ejecutar()
     {
-        Console.Clear();
-        Console.WriteLine("Has seleccionado la Opción 1.");
-        // Aquí va la lógica de la página o módulo.
+        var continuar = true;
+        while (continuar)
+        {
+            Console.Clear();
+
+          
+
+            Console.WriteLine(" ");
+            Utilidades.CenterText(" Menu De Huellas Dactilares ");
+            Console.WriteLine(" ");
+            Console.WriteLine("1. Capturar Huella");
+            Console.WriteLine("2. Convertir a condigo QR");
+            Console.WriteLine("3. Registar Huella");
+            Console.WriteLine("4. Verificar Huella");
+            Console.WriteLine("0. Salir");
+            Console.Write("Seleccione una opción: ");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "3":
+                    RegistrarHuella();
+                    break;
+                case "2":
+                   
+                    break;
+                case "1":
+                    
+                    break;
+                case "0":
+                    continuar = false;
+                    break;
+                case "4":
+
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Intente de nuevo.");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+    private void RegistrarHuella() {
+        Console.WriteLine("Registrar huella");
         Console.ReadKey();
     }
+
+
 }
